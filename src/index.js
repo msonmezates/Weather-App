62bb5a209e893ad9476b1964deac674fb5357281
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-25 11:09:39 -0600

    Add redux boiler plate and combine with React

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <App />
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
   ,document.getElementById('root')
 );
