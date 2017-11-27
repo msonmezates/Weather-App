804f1dd4336338a1128be076ba5d7fe624b2199f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-27 12:22:58 -0600

    Install and use redux-promise middleware to call ajax

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import App from './components/app';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import ReduxPromise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
