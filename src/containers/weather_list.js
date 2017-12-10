4bf03cc32bfef433f36211ce8901550c310e0452
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-12-09 18:59:56 -0600

    Display units for temperature, pressure and humidity

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -14,13 +14,13 @@ class WeatherList extends Component {
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Chart data={temps} color="orange" />
+          <Chart data={temps} color="orange" units="K" />
         </td>
         <td>
-          <Chart data={pressures} color="blue" />
+          <Chart data={pressures} color="blue" units="hPa" />
         </td>
         <td>
-          <Chart data={humidities} color="red" />
+          <Chart data={humidities} color="red" units="%" />
         </td>
       </tr>
     );
@@ -31,9 +31,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
