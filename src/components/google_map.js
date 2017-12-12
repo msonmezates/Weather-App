c8aad3f6fe1708d96e053a61517565a4bfd7415f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-12-11 19:02:24 -0600

    Integrate google maps

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default (props) => {
+
+  componentDidMount() {
+    new google.maps.Map(refs.map, {
+      zoom: 12,
+      center: {
+        lat: props.lat,
+        lng: props.lon
+      }
+    });
+  }
+
   return (
-    <div>
-      Google Map Component  
-    </div>
+    <div ref="map" />
   );
 }
